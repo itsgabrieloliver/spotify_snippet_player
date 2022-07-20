96a7f92dce869c6311c8a0458ad1c5fd2085882e
Lunar | Neptr <straxxmusicteam@gmail.com>
2022-07-20 01:00:15 -0400

    add config file and remove unnecessary code

--- src/App.jsx
+++ src/App.jsx
@@ -2,22 +2,19 @@ import React, { useState, useEffect } from 'react'
 
 import Player from './Player'
 
+import * as CONF from '../config'
+
 const App = () => {
-	const CLIENT_ID = '9d32130a4f3b4402b73b5d55566fca05'
-	const REDIRECT_URI = 'http://localhost:3000'
-	const AUTH_ENDPOINT = 'https://accounts.spotify.com/authorize'
-	const RESPONSE_TYPE = 'token'
+	const CLIENT_ID = CONF.SPOTIFY_CLIENT_ID
+	const REDIRECT_URI = CONF.REDIRECT_URI
+	const AUTH_ENDPOINT = CONF.AUTH_ENDPOINT
+	const RESPONSE_TYPE = CONF.RESPONSE_TYPE
 
 	const scopes = [
-		'user-modify-playback-state',
-		'user-read-playback-state',
-		'user-read-email',
-		'user-read-recently-played',
 		'user-top-read'
 	]
-
+	
 	const [token, setToken] = useState(null)
-	const [userId, setUserId] = useState(null)
 
 	useEffect( () => {
 		const hash = window.location.hash
@@ -29,20 +26,12 @@ const App = () => {
 			window.localStorage.setItem('token', token)
 		}
 		setToken(token)
-		fetch(`https://api.spotify.com/v1/me?access_token=${token}`, {
-			method: 'GET'
-		})
-			.then((res) => res.json())
-			.then((data) => {
-				console.log(data)
-				setUserId(data.id)
-			})
 	}, [])
 
 	return (
 		<div className={'w-full h-screen bg-gray-900 flex justify-center items-center'}>
 			{!token && <a className={'px-4 py-3 bg-gradient-to-br from-teal-300 to-green-400 rounded-full text-white text-sm tracking-wide font-bold uppercase hover:-translate-y-1 hover:origin-center hover:rotate-2'} href={`${AUTH_ENDPOINT}?client_id=${CLIENT_ID}&redirect_uri=${REDIRECT_URI}&response_type=${RESPONSE_TYPE}&scope=${scopes.join('%20')}`}>Login with Spotify</a> }
-			{token && <Player token={token} userId={userId} />}
+			{token && <Player token={token} />}
 		</div>
 	)
 }
