787c35acb4736d8770e013421ebe600b08e1a3c3
Gabriel Oliver <straxxmusicteam@gmail.com>
2022-07-24 12:16:31 -0400

    update example config

--- config/config.example.ts
+++ config/config.example.ts
@@ -1,4 +1,4 @@
 export const CLIENT_ID: string = ''
-export const REDIRECT_URI: string = ''
-export const AUTH_ENDPOINT: string = ''
-export const RESPONSE_TYPE: string = ''
+export const REDIRECT_URI: string = 'http://localhost:3000'
+export const AUTH_ENDPOINT: string = 'https://accounts.spotify.com/authorize'
+export const RESPONSE_TYPE: string = 'token'
